708d6512097202ed68a6bed01597b9c884024ea8
Sagar Routh <sagarrouth0512@gmail.com>
2017-01-12 07:20:26 +0530

    Rectified some syntax errors in the file

--- app.js
+++ app.js
@@ -10,19 +10,21 @@ app.use(bodyParser.urlencoded({limit:'10mb', extended:true}));
 
 
 try {
-  fs.readdirsync('./app/models').forEach(function (fileName) {
+  fs.readdirSync('./app/models').forEach(function (fileName) {
     if(fileName.indexOf('.js')>-1)
       require(path.join('./app/models',fileName));
   });
 
-  fs.readdirsync('./app/controllers').forEach(function (fileName) {
+  fs.readdirSync('./app/controllers').forEach(function (fileName) {
     if(fileName.indexOf('.js')>-1){
-      var route = require(path.join('./app/models',fileName));
+      var route = require(path.join(__dirname,'./app/controllers',fileName));
       route.controller(app);
     }
   });
 } catch (error) {
   console.log(error.message);
 } finally {
-
+  app.listen(3000, function () {
+    console.log('server started');
+  });
 }

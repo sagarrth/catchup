46b027b41386fc1bad732bc573b2124d60bfb4ab
Sagar Routh <Sagar Routh>
2017-02-12 22:54:28 +0530

    Removed unique property on name fields

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -4,8 +4,8 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
   userName        : {type:String, unique:true, required:true, trim:true},
-  firstName       : {type:String, unique:true, required:true, trim:true},
-  lastName        : {type:String, unique:true, required:true, trim:true},
+  firstName       : {type:String, required:true, trim:true},
+  lastName        : {type:String, required:true, trim:true},
   email           : {type:String, unique:true, required:true, trim:true},
   phone           : {type:Number, unique:true, required:true, trim:true},
   password        : {type:String, required:true}
